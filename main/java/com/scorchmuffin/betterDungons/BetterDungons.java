4b1fd79fc0359d079c6691b95d9d05a8b178ee2f
quingtaylor <quinlan@Amary-Taylors-MacBook-Pro.local>
2016-05-21 12:02:03 -0700

    Setup workspace

---
+++ main/java/com/scorchmuffin/betterDungons/BetterDungons.java
@@ -0,0 +1,9 @@
+package com.scorchmuffin.betterDungons;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = BetterDungons.MODID, version = BetterDungons.VERSION)
+public class BetterDungons {
+	public static final String MODID = "BD";
+    public static final String VERSION = "0.0.1";
+}
